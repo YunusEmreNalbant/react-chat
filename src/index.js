f290dbb113155b4dfc48a401c5e904e6eaf17460
YunusEmreNalbant <yunus.nalbant0@gmail.com>
2021-02-06 16:58:58 +0300

    redux store

--- src/index.js
+++ src/index.js
@@ -1,22 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import firebase from "./firebase"
+import {Provider} from 'react-redux'
+import {
+    ReactReduxFirebaseProvider,
+    firebaseReducer
+} from 'react-redux-firebase'
 import 'semantic-ui-css/semantic.min.css'
 import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import SignUp from "./components/auth/SignUp";
 import Login from "./components/auth/Login";
+import store from "./components/store/store";
 
+const rrfConfig = {
+    userProfile: 'users'
+};
 
+const rrfProps = {
+    firebase,
+    config: rrfConfig,
+    dispatch: store.dispatch
+};
 const Root = () => (
-    <Router>
-        <Switch>
-            <Route exact path="/" component={App} />
-            <Route path="/signup" component={SignUp} />
-            <Route path="/login" component={Login} />
-        </Switch>
-    </Router>
-)
+    <Switch>
+        <Route exact path="/" component={App}/>
+        <Route path="/signup" component={SignUp}/>
+        <Route path="/login" component={Login}/>
+    </Switch>
+);
 
-ReactDOM.render(<Root/>, document.getElementById('root'));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <ReactReduxFirebaseProvider {...rrfProps}>
+            <Router>
+                <Root/>
+            </Router>
+        </ReactReduxFirebaseProvider>
+    </Provider>
+    , document.getElementById('root'));
