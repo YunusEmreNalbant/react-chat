dac5b0dd56892cc939fb1028a8e78bedb58afa81
YunusEmreNalbant <yunus.nalbant0@gmail.com>
2021-02-07 14:29:12 +0300

    private route

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import firebase from "./firebase"
+
 import {Provider} from 'react-redux'
 import {
     ReactReduxFirebaseProvider,
     firebaseReducer
 } from 'react-redux-firebase'
 import 'semantic-ui-css/semantic.min.css'
-import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch, Redirect, useHistory} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import SignUp from "./components/auth/SignUp";
 import Login from "./components/auth/Login";
 import store from "./components/store/store";
-
+import PrivateRoute from "./components/auth/PrivateRoute";
 const rrfConfig = {
     userProfile: 'users'
 };
@@ -23,13 +24,28 @@ const rrfProps = {
     config: rrfConfig,
     dispatch: store.dispatch
 };
-const Root = () => (
-    <Switch>
-        <Route exact path="/" component={App}/>
-        <Route path="/signup" component={SignUp}/>
-        <Route path="/login" component={Login}/>
-    </Switch>
-);
+
+const Root = () => {
+    const history = useHistory();
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                history.push('/');
+            } else {
+                history.push('/login');
+            }
+        })
+    }, []);
+    return (
+        <Switch>
+            <PrivateRoute exact path="/">
+                <App/>
+            </PrivateRoute>
+            <Route path="/signup" component={SignUp}/>
+            <Route path="/login" component={Login}/>
+        </Switch>
+    )
+};
 
 
 ReactDOM.render(
