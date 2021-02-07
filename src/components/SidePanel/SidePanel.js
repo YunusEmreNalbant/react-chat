9459df815167a221136a25c9bde3481fd8b1da47
YunusEmreNalbant <yunus.nalbant0@gmail.com>
2021-02-07 15:40:52 +0300

    sidepanel, new channel

---
+++ src/components/SidePanel/SidePanel.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import {Popup, Menu, Icon} from "semantic-ui-react";
+import CreateChannelForm from "../Channels/CreateChannelForm";
+
+const SidePanel = () => {
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
+    return (
+        <>
+            <Menu vertical inverted secondary fixed={"left"} color={"blue"}
+                  style={{width: "346px", fontSize: "1.3rem"}}>
+                <Menu.Item>
+
+                </Menu.Item>
+                <Menu.Item>
+                    <Menu.Header>
+                        Kanallar
+                        <span style={{float: "right"}}>
+                        <Popup content={"Yeni Kanal Oluştur"}
+                               trigger={<Icon name={"add"} onClick={(event) => handleOpen()}/>}>
+
+                        </Popup>
+                    </span>
+                    </Menu.Header>
+                    {[...new Array(10)].map((prop, index) => (
+                        <Menu.Item key={index} name={"A"} as={"a"} icon={"hashtag"}/>
+                    ))}
+                </Menu.Item>
+            </Menu>
+
+            <CreateChannelForm open={open} onOpen={handleOpen} onClose={handleClose}/>
+        </>
+    );
+};
+
+export default SidePanel;
