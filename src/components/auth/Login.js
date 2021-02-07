380f6bfea1f19d770ad7a583b9dd5803f1231cf2
YunusEmreNalbant <yunus.nalbant0@gmail.com>
2021-02-07 13:41:51 +0300

    firebase authentication

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,11 +1,17 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+import {useFirebase} from "react-redux-firebase";
+
 import {useForm} from "react-hook-form";
 import {Form, Segment, Button, Grid, Message} from "semantic-ui-react";
 import styles from "./auth.module.css";
 import {Link} from "react-router-dom";
 
 const Login = () => {
+    const firebase = useFirebase();
+
     const {register, errors, handleSubmit, setValue} = useForm();
+    const [fbErrors, setFbErrors] = useState([]);
+    const [submitting, setSubmitting] = useState(false);
 
     useEffect(() => {
         register({name: "email"}, {required: true});
@@ -13,9 +19,22 @@ const Login = () => {
     }, []);
 
 
-    const onSubmit = (data,e) => {
-       console.log(data);
+    const onSubmit = ({email, password}, e) => {
+        setSubmitting(true);
+        setFbErrors([]);
+
+        firebase.login({
+            email, password
+        }).then((data) => {
+            console.log(data);
+        }).catch((error) => {
+            setFbErrors([{message: error.message}]);
+        }).finally(()=>setSubmitting(false))
     };
+
+
+    const displayErrors = () => fbErrors.map((error, index) => <p key={index}> {error.message}</p>);
+
     return (
         <Grid textAlign={"center"} verticalAlign={"middle"} className={styles.container}>
             <Grid.Column style={{maxWidth: 450}}>
@@ -23,8 +42,8 @@ const Login = () => {
                 <Form size={"large"} className={styles.form} onSubmit={handleSubmit(onSubmit)}>
                     <Segment>
                         <Form.Input fluid icon={"mail"} iconPosition={"left"} name={"email"}
-                                    onChange={(event, {name,value})=>{
-                                        setValue(name,value)
+                                    onChange={(event, {name, value}) => {
+                                        setValue(name, value)
                                     }}
                                     placeholder={"Email Adresi"} type={"email"}
                                     error={errors.email ? true : false}
@@ -35,9 +54,14 @@ const Login = () => {
                                     }}
                                     placeholder={"Şifre"} type={"password"}/>
 
-                        <Button  color={"purple"} fluid size={"large"}>Giriş Yap</Button>
+                        <Button disabled={submitting} color={"purple"} fluid size={"large"}>Giriş Yap</Button>
                     </Segment>
                 </Form>
+                {
+                    fbErrors.length > 0 && (
+                        <Message error>{displayErrors()}</Message>
+                    )
+                }
                 <Message>
                     Yeni misin ? <Link to={"/signup"}>Hesap Oluştur</Link>
                 </Message>
--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,12 +1,15 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+import {useFirebase} from "react-redux-firebase";
 import {Form, Segment, Button, Grid, Message} from "semantic-ui-react";
 import styles from "./auth.module.css";
 import {Link} from "react-router-dom";
 import {useForm} from "react-hook-form";
 
 const SignUp = () => {
+    const firebase = useFirebase();
     const {register, errors, handleSubmit, setValue} = useForm();
-
+    const [fbErrors, setFbErrors] = useState([]);
+    const [submitting, setSubmitting] = useState(false);
     useEffect(() => {
         register({name: "username"}, {required: true});
         register({name: "email"}, {required: true});
@@ -14,9 +17,27 @@ const SignUp = () => {
     }, []);
 
 
-    const onSubmit = (data, e) => {
-        console.log(data)
+    const onSubmit = ({username, email, password}, e) => {
+        setSubmitting(true);
+        setFbErrors([]);
+
+        const [first, last] = username.split(' ');
+        firebase.createUser(
+            {email, password},
+            {
+                name: username,
+                avatar: `https://ui-avatars.com/api/?name=${first}+${last}&background=random&color=fff`
+            }
+        ).then((user) => {
+            console.log(user);
+        }).catch((error) => {
+            console.log(error);
+            setFbErrors([{message: error.message}]);
+        }).finally(()=>setSubmitting(false))
     };
+
+    const displayErrors = () => fbErrors.map((error, index) => <p key={index}> {error.message}</p>);
+
     return (
         <Grid textAlign={"center"} verticalAlign={"middle"} className={styles.container}>
             <Grid.Column style={{maxWidth: 450}}>
@@ -46,9 +67,14 @@ const SignUp = () => {
                                     error={errors.password ? true : false}
                         />
 
-                        <Button color={"purple"} fluid size={"large"}>Kaydol</Button>
+                        <Button disabled={submitting} color={"purple"} fluid ize={"large"}>Kaydol</Button>
                     </Segment>
                 </Form>
+                {
+                    fbErrors.length > 0 && (
+                        <Message error>{displayErrors()}</Message>
+                    )
+                }
                 <Message>
                     Zaten bir hesabın var mı? <Link to={"/login"}>Giriş Yap</Link>
                 </Message>
