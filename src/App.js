db319c457d0322c49b4fee2f54fb14a343f646b0
YunusEmreNalbant <yunus.nalbant0@gmail.com>
2021-02-08 14:34:01 +0300

    kanal mesajı

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import {Button, Grid} from 'semantic-ui-react'
 import SidePanel from "./components/SidePanel/SidePanel";
-
+import ChatPanel from "./components/ChatPanel/ChatPanel";
+import {useSelector} from "react-redux";
 const App = () => {
+    const currentChannel = useSelector(state=> state.channels.currentChannel)
     return (
         <Grid columns={2} style={{background:"#eee",height:"110vh"}}>
-            <Grid.Column width={"3"} style={{background:"#000"}}>
+            <Grid.Column width={"3"} >
                 <SidePanel/>
             </Grid.Column>
-            <Grid.Column width={"13"} style={{background:"#eee"}}>
-
+            <Grid.Column width={"13"} >
+                {currentChannel && <ChatPanel currentChannel={currentChannel} />}
             </Grid.Column>
         </Grid>
     );
